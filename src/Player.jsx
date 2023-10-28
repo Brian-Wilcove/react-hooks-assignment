5b350e62f32776265b17df6e7f53e96be9a58a75
Brian Wilcove <99514411+Brian-Wilcove@users.noreply.github.com>
2023-10-28 16:41:54 -0400

    Finished Component 2...apparently messed up the useState call to be useState([0]) instead of useState (0)

--- src/Player.jsx
+++ src/Player.jsx
@@ -1,6 +1,16 @@
 import React from "react";
 import { useState } from "react";
 function Player({name}) {
+  const [score, setScore] = useState(0);
+
+  function addPoint() {
+    setScore(score + 1);
+  }
+
+  function removePoint() {
+    setScore(score - 1);
+  }
+
   return (
     <div className="container-fluid border border-dark p-3 m-3">
       <div className="row justify-content-center">
@@ -8,16 +18,18 @@ function Player({name}) {
         <h2 className="text-center"></h2>
       </div>
       <div className="row justify-content-center">
-        <p className="text-center fs-3">{/* Render score here */}</p>
+        <p className="text-center fs-3">{/* Render score here */}{score}</p>
       </div>
       <div className="row justify-content-center">
         <div className="col-md-6">
           {/* Add increment event handler */}
-          <button className="btn btn-outline-primary w-100">Add Point +</button>
+          <button className="btn btn-outline-primary w-100" onClick={addPoint}>
+            Add Point +
+          </button>
         </div>
         <div className="col-md-6">
           {/* Add decrement event handler */}
-          <button className="btn btn-outline-danger w-100">
+          <button className="btn btn-outline-danger w-100" onClick={removePoint}>
             Remove Point -
           </button>
         </div>
